1df0fa633e0f4462b83c7db80ea4c1560ae1434e
md motailab <mdmotailab@gmail.com>
2021-04-21 17:05:58 +0600

    updated static file settings

--- weather/urls.py
+++ weather/urls.py
@@ -21,5 +21,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('', include('app.urls')),
     path('admin/', admin.site.urls),
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+]
 
+if settings.DEBUG:
+    urlpatterns + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
