dfdd61231369e38a7b53c42ac625e35a3f4f9661
motailab <mdmotailab@gmail.com>
2021-08-01 00:22:07 +0600

    changed static root

--- weather/settings.py
+++ weather/settings.py
@@ -125,7 +125,7 @@ STATICFILES_DIRS = (
     BASE_DIR / 'static',
 )
 
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATIC_ROOT = BASE_DIR / 'static'
 
 MEDIA_ROOT = BASE_DIR / 'media'
 # Default primary key field type
