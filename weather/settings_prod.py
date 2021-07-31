b9a468d374f87be89d4fb27b598797c0b8b5f38d
motailab <mdmotailab@gmail.com>
2021-07-31 23:34:46 +0600

    updated production settings.

--- weather/settings_prod.py
+++ weather/settings_prod.py
@@ -4,9 +4,9 @@ from decouple import config
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': config('DB_NAME', cast='str', default='weather'),
-        'USER': config('DB_USER', cast='str', default='weatheruser'),
-        'PASSWORD': config('DB_PASSWORD', cast='str', default='weatherpassword'),
+        'NAME': config('DB_NAME', cast=str),
+        'USER': config('DB_USER', cast=str),
+        'PASSWORD': config('DB_PASSWORD', cast=str),
         'HOST': 'localhost',
         'PORT': '',
     }
